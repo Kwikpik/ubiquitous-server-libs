09772ed2848b4fb24edba3134a5848a256cd8de6
Kingsley Victor <javaprodigy56@gmail.com>
2024-11-26 17:07:08 +0100

    chore: include whatsapp option for sms

--- src/utils/sms.ts
+++ src/utils/sms.ts
@@ -11,17 +11,19 @@ export const initializeSMSSender = (apiKey?: string) => {
   return api;
 };
 
-export const send = async (to: string | string[], sms: string): Promise<MessageResponse> => {
+export const send = async (to: string | string[], sms: string): Promise<MessageResponse[]> => {
   assert.ok(!isNil(api), "sms_api_not_initialized");
 
   const isNG =
     (typeof to === "string" && (to.startsWith("+234") || to.startsWith("234"))) ||
     (Array.isArray(to) && to.every(x => x.startsWith("+234") || x.startsWith("234")));
-  const from = isNG ? "N-Alert" : "Kwik Pik";
-  const channel = isNG ? "dnd" : "generic";
+  const from = isNG ? "N-Alert" : "Kwikpik";
+  const channel = isNG ? ["dnd", "whatsapp"] : ["generic", "whatsapp"];
 
   try {
-    const res = await api.messaging({ to, sms, channel, from }).send();
+    const res = await Promise.all(
+      channel.map(ch => api.messaging({ to, sms, channel: ch as "generic" | "whatsapp" | "dnd", from }).send())
+    );
     return res;
   } catch (error) {
     return await Promise.reject(error);
